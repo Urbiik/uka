42e104f1ce4b7e78c25b6e6bb54c3340d4cd2915
Uroš Regodić <uregodic@smart.edu.rs>
2022-01-24 09:08:15 +0100

    Init commit

---
+++ prvi_cas.js
@@ -0,0 +1,12 @@
+var c = [1,3,4]
+undefined
+var g = [3,4,1]
+undefined
+b = 3
+3
+if (b>2)
+    
+VM194:2 Uncaught SyntaxError: Unexpected end of input
+if (b > 2)
+    b = "neki string"
+'neki string'
